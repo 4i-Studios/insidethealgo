6fe5ce128f73080565ca0ff1101ae32404f927cd
Maricbrass <Maricbrass@users.noreply.github.com>
2025-09-20 18:34:05 +0000

    deploy: 9be44350d4ad3f86cafcb15d7cbebe2596d198f7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "401843833"
+    serviceWorkerVersion: "3271529356"
   }
 });
